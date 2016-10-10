5f77f53c8b1c69f49b2b21e4008df3dc22c3aa73
Cody Brunner <rockchalkwushock@icloud.com>
2016-10-10 17:06:40 -0500

    V1.0: Initial files & dependencies loaded. Initial build complete and ran 100%.

---
+++ webpack.config.babel.js
@@ -0,0 +1,26 @@
+import path from 'path';
+import webpack from 'webpack';
+import packageData from './package.json';
+
+let filename = [packageData.name, packageData.version, 'js'];
+
+module.exports = {
+    entry: path.resolve(__dirname, packageData.main),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: filename.join('.'),
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
