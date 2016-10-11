cdc3d9c2d03d2af808abb9f9fb11d828ea109fd8
Cody Brunner <rockchalkwushock@icloud.com>
2016-10-11 12:24:29 -0500

    V2.0: Added /components & /test. Added component files and template code. Added dependencies. Updated webpack.config.babel.js. Added test-index.js.

---
+++ components/List.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Card from './Card';
+import Button from './Button';
+
+export default List;
---
+++ test/test-index.js
@@ -0,0 +1,2 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
