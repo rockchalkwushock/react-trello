eda1a486b3a31fd3d4bbe061c3763b22b5cb92af
Cody Brunner <rockchalkwushock@icloud.com>
2016-10-11 12:49:50 -0500

    V2.3: Added Card & Button components. Cleaned out index.js.

--- components/Button.js
+++ components/Button.js
@@ -1,3 +1,18 @@
+/*
+  This is the Button component.
+  This is a stateless component.
+  Data is passed to it through 'props'.
+  Data is passed from the Parent Component: List.
+  Button is controlled by the event: onClick.
+*/
+
+
 import React from 'react';
+
+let Button = (props) => {
+  return (
+    <button onClick={props.onClick}>Add Card</button>;
+  );
+};
 
 export default Button;
--- components/Card.js
+++ components/Card.js
@@ -1,3 +1,20 @@
+/*
+  This is the Card component.
+  This is a stateless component.
+  Data is passed to it through 'props'.
+  Data is passed from the Parent Component: List.
+*/
+
 import React from 'react';
+
+let Card = () => {
+  let hello = 'This is a card';
+  return (
+    <div className='card'>
+      <p>{props.title}</p>
+      <p>{props.content}</p>
+    </div>
+  );
+};
 
 export default Card;
